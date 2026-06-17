39473743ed4d341b77ce4b3ce5dace46b40bd46a
cyberdave80 <davidpge12@gmail.com>
2026-06-17 17:08:59 -0400

    changed cleanup.bash as well for the new manifest

--- Question-8-CNI-Network-Policy/cleanup.bash
+++ Question-8-CNI-Network-Policy/cleanup.bash
@@ -4,7 +4,7 @@ set -uo pipefail
 echo "Cleaning up Question 8: CNI & Network Policy..."
 
 # Remove Calico/tigera-operator if installed
-kubectl delete -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/tigera-operator.yaml 2>/dev/null || true
+kubectl delete -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/calico.yaml 2>/dev/null || true
 kubectl delete namespace tigera-operator --ignore-not-found
 kubectl delete namespace calico-system --ignore-not-found
 
